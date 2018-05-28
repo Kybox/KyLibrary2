ed567d5b44d6b3910b63521f2deedf47d7066f17
Kybox <nslr@riseup.net>
2018-05-28 20:17:04 +0200

    Update User entity

--- webservice/src/main/java/fr/kybox/entities/User.java
+++ webservice/src/main/java/fr/kybox/entities/User.java
@@ -1,8 +1,6 @@
 package fr.kybox.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 
 /**
@@ -12,7 +10,15 @@ import java.sql.Date;
 
 @Entity
 @Table(name = "user_account", schema = "public")
+@NamedQueries({
+        @NamedQuery(name = User.GET_USER_BY_ID, query = "SELECT u FROM User u WHERE u.id = :id"),
+        @NamedQuery(name = User.GET_USER_BY_LOGIN, query = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
+})
+
 public class User extends AbstractEntity {
+
+    public static final String GET_USER_BY_ID = "User.GetUserByID";
+    public static final String GET_USER_BY_LOGIN = "User.GetUserByLogin";
 
     @Column
     private String email;
