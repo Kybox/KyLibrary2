ae53492157680bed04b95781dcd9342cb037aa45
Kybox <nslr@riseup.net>
2018-05-28 12:11:47 +0200

    Add DAO interface

---
+++ webservice/src/main/java/fr/kybox/dao/IDao.java
@@ -0,0 +1,15 @@
+package fr.kybox.dao;
+
+import fr.kybox.entities.Book;
+import fr.kybox.entities.User;
+
+import java.util.List;
+
+public interface IDao {
+
+    User getUser(Integer id);
+
+    List<Book> getUserBookList(User user);
+
+    List<Book> searchBook(String keyword);
+}
