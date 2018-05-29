d38e46a12c20f7886bff4b2f111bffe2d28d9076
Kybox <nslr@riseup.net>
2018-05-29 09:55:41 +0200

    Add XMLGregorianCalendar converter

---
+++ webservice/src/main/java/fr/kybox/utils/Converter.java
@@ -0,0 +1,30 @@
+package fr.kybox.utils;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.sql.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * @author Kybox
+ * @version 1.0
+ */
+public class Converter {
+
+    public static XMLGregorianCalendar SQLDateToXML(Date SQLDate){
+
+        XMLGregorianCalendar xmlDate = null;
+
+        try {
+            GregorianCalendar gregorianCalendar = new GregorianCalendar();
+
+            gregorianCalendar.setTime(SQLDate);
+            xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+
+        }
+        catch (DatatypeConfigurationException e) { e.printStackTrace(); }
+
+        return xmlDate;
+    }
+}
