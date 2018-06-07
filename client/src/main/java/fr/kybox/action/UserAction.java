8aeaf74a017033b893dca5aff2152046908c4eec
Kybox <nslr@riseup.net>
2018-06-07 10:42:54 +0200

    Update Client UserAction class

--- client/src/main/java/fr/kybox/action/UserAction.java
+++ client/src/main/java/fr/kybox/action/UserAction.java
@@ -1,10 +1,26 @@
 package fr.kybox.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import fr.kybox.gencode.User;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
 
 /**
  * @author Kybox
  * @version 1.0
  */
-public class UserAction extends ActionSupport {
+public class UserAction extends ActionSupport implements SessionAware {
+
+    private User user;
+    private Map<String, Object> session;
+
+    public User getUser() {
+        return (User) session.get("user");
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
 }
