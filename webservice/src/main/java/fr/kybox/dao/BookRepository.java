45275a35b4b003bc8c82d6ea09166b78a09ba403
Kybox <nslr@riseup.net>
2018-06-14 13:18:03 +0200

    Update WebService DAO search book

--- webservice/src/main/java/fr/kybox/dao/BookRepository.java
+++ webservice/src/main/java/fr/kybox/dao/BookRepository.java
@@ -3,6 +3,8 @@ package fr.kybox.dao;
 import fr.kybox.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Kybox
  * @version 1.0
@@ -10,4 +12,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
 
     BookEntity findByIsbn(String isbn);
+    List<BookEntity> findAllByTitleIgnoreCaseContaining(String keyword);
 }
