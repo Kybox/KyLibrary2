754c489ae798b986ab39058e0225d980e870c14c
Kybox <nslr@riseup.net>
2018-06-17 13:14:43 +0200

    Update WebService Converter class (SQLDate to calendar)

--- webservice/src/main/java/fr/kybox/utils/Converter.java
+++ webservice/src/main/java/fr/kybox/utils/Converter.java
@@ -1,16 +1,7 @@
 package fr.kybox.utils;
 
-import org.joda.time.LocalDate;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.sql.Date;
-import java.sql.SQLData;
-import java.time.ZoneId;
-import java.util.GregorianCalendar;
-
-import static java.time.ZoneId.systemDefault;
+import java.util.Calendar;
 
 /**
  * @author Kybox
@@ -18,37 +9,15 @@ import static java.time.ZoneId.systemDefault;
  */
 public class Converter {
 
-    public static XMLGregorianCalendar SQLDateToXML(Date SQLDate){
+    public static Calendar SQLDateToCalendar(Date date){
 
-        XMLGregorianCalendar xmlDate = null;
-
-        if(SQLDate != null) {
-
-            try {
-                GregorianCalendar gregorianCalendar = new GregorianCalendar();
-
-                gregorianCalendar.setTime(SQLDate);
-                xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
-
-            } catch (DatatypeConfigurationException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return xmlDate;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
     }
 
     public static Date DateToSQLDate(java.util.Date date){
 
         return new Date(date.getTime());
-    }
-
-    public static XMLGregorianCalendar LocalDateToXML(LocalDate localDate){
-
-        java.util.Date date = localDate.toDate();
-
-        Date sqlDate = new Date(date.getTime());
-
-        return SQLDateToXML(sqlDate);
     }
 }
