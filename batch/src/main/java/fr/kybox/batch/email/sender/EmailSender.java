a1f03ba108b34739dc622b3ec93023c733130484
Kybox <nslr@riseup.net>
2018-06-29 03:55:09 +0200

    Remove Maven batch module

--- batch/src/main/java/fr/kybox/batch/email/sender/EmailSender.java
+++
@@ -1,8 +0,0 @@
-package fr.kybox.batch.email.sender;
-
-import fr.kybox.batch.bean.EmailMessage;
-
-public interface EmailSender {
-
-    void send(EmailMessage email);
-}
--- batch/src/main/java/fr/kybox/batch/job/execution/listener/JobExecutionListener.java
+++
@@ -1,9 +0,0 @@
-package fr.kybox.batch.job.execution.listener;
-
-import org.springframework.batch.core.JobExecution;
-
-public interface JobExecutionListener {
-
-    void beforeJob(JobExecution jobExecution);
-    void afterJob(JobExecution jobExecution);
-}
