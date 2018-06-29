0f2d428c4ffb73f79f901d5e1bb766accdaec729
Kybox <nslr@riseup.net>
2018-06-29 03:45:29 +0200

    Update WebService BorrowedBooksRepo (rename find method)

--- webservice/src/main/java/fr/kybox/dao/BorrowedBooksRepository.java
+++ webservice/src/main/java/fr/kybox/dao/BorrowedBooksRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface BorrowedBooksRepository extends JpaRepository<BorrowedBook, Integer> {
 
     List<BorrowedBook> findAllByUserOrderByReturnedDesc(UserEntity user);
-    BorrowedBook findByUserAndBookOrderByReturnDateDesc(UserEntity user, BookEntity bookEntity);
+    BorrowedBook findByUserAndBook(UserEntity user, BookEntity bookEntity);
 }
