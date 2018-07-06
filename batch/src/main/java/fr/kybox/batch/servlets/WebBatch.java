bd6ca0fee3f847c6b674c1fa85435d4490cbb11c
Kybox <nslr@riseup.net>
2018-07-06 08:57:02 +0200

    Update Batch servlet

--- batch/src/main/java/fr/kybox/batch/servlets/WebBatch.java
+++ batch/src/main/java/fr/kybox/batch/servlets/WebBatch.java
@@ -1,6 +1,6 @@
 package fr.kybox.batch.servlets;
 
-import fr.kybox.batch.tasklet.TestTasklet;
+import fr.kybox.batch.Reminder;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.servlet.ServletException;
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author Kybox
@@ -31,8 +32,9 @@ public class WebBatch extends HttpServlet {
         String password = request.getParameter("password");
 
         try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-job.xml")) {
-            TestTasklet testTasklet = context.getBean(TestTasklet.class);
-            testTasklet.run();
+            Reminder reminder = context.getBean(Reminder.class);
+            Map<String, Object> resultMap = reminder.run(email, password);
+            request.setAttribute("result", resultMap);
         }
 
         this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
