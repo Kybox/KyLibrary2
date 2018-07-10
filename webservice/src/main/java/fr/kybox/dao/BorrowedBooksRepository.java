d40366dbea3da578d583faba1fd1825a5af3ae59
Kybox <nslr@riseup.net>
2018-07-10 13:22:14 +0200

    Update WebService DAO BorrowedBookRepository

--- webservice/src/main/java/fr/kybox/dao/BorrowedBooksRepository.java
+++ webservice/src/main/java/fr/kybox/dao/BorrowedBooksRepository.java
@@ -5,10 +5,12 @@ import fr.kybox.entities.BorrowedBook;
 import fr.kybox.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BorrowedBooksRepository extends JpaRepository<BorrowedBook, Integer> {
 
     List<BorrowedBook> findAllByUserOrderByReturnedDesc(UserEntity user);
     BorrowedBook findByUserAndBook(UserEntity user, BookEntity bookEntity);
+    Iterable<BorrowedBook> findAllByReturnDateAfterAndReturnedFalse(Date date);
 }
