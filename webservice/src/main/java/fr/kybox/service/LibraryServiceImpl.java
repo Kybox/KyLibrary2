c70194d8e649809023d788346d8092c7bf0b4682
Kybox <nslr@riseup.net>
2018-11-03 17:15:53 +0100

    Update WSDL location in @WebService

--- webservice/src/main/java/fr/kybox/service/LibraryServiceImpl.java
+++ webservice/src/main/java/fr/kybox/service/LibraryServiceImpl.java
@@ -33,7 +33,7 @@ import java.util.*;
         name = "LibraryWebService",
         targetNamespace = "dd7b026a-d6a2-4089-adb2-596ab0598c73",
         portName = "LibraryService",
-        wsdlLocation = "https://raw.githubusercontent.com/Kybox/KyLibrary/master/webservice/src/main/resources/wsdl/LibraryService.wsdl")
+        wsdlLocation = "https://raw.githubusercontent.com/Kybox/KyLibrary2/master/webservice/src/main/resources/wsdl/LibraryService.wsdl")
 public class LibraryServiceImpl extends SpringBeanAutowiringSupport implements LibraryService {
 
     private final static Logger logger = LogManager.getLogger(LibraryServiceImpl.class);
