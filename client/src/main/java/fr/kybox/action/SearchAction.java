83cee8fee8583bdf9540521242595fa760c801cf
kybox <nslr@riseup.net>
2018-11-08 11:16:53 +0100

    #1 Add SessionAware implementation to the SearchAction class

--- client/src/main/java/fr/kybox/action/SearchAction.java
+++ client/src/main/java/fr/kybox/action/SearchAction.java
@@ -3,17 +3,20 @@ package fr.kybox.action;
 import com.opensymphony.xwork2.ActionSupport;
 import fr.kybox.gencode.*;
 import fr.kybox.utils.ServiceFactory;
+import org.apache.struts2.interceptor.SessionAware;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kybox
  * @version 1.0
  */
-public class SearchAction extends ActionSupport {
+public class SearchAction extends ActionSupport implements SessionAware {
 
     private String keyword;
     private List<Book> bookList;
+    private Map<String, Object> session;
 
     public String getKeyword() { return keyword; }
     public void setKeyword(String keyword) { this.keyword = keyword; }
@@ -36,5 +39,10 @@ public class SearchAction extends ActionSupport {
         }
 
         return ActionSupport.SUCCESS;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
     }
 }
