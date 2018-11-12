13e89dd39fc96a598f6a1cb27d2318b4931d34a2
kybox <nslr@riseup.net>
2018-11-12 19:08:42 +0100

    (Client) Update UserAction class

--- client/src/main/java/fr/kybox/action/UserAction.java
+++ client/src/main/java/fr/kybox/action/UserAction.java
@@ -2,7 +2,11 @@ package fr.kybox.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import fr.kybox.gencode.*;
+import fr.kybox.security.Token;
+import fr.kybox.utils.ResultCode;
 import fr.kybox.utils.ServiceFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts2.interceptor.SessionAware;
 
 import java.text.DateFormat;
@@ -21,24 +25,53 @@ public class UserAction extends ActionSupport implements SessionAware {
     private String tab;
     private String isbn;
     private Map<String, Object> session;
+    private List<BookBorrowed> borrowedBooks;
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    public String execute() {
+
+        logger.info("Execute method call");
+
+        String result = ActionSupport.SUCCESS;
+
+        LibraryService service = ServiceFactory.getLibraryService();
+
+        UserBookList userBookList = new UserBookList();
+        userBookList.setToken(Token.getToken());
+
+        UserBookListResponse userBookListResponse = service.userBookList(userBookList);
+
+        if(userBookListResponse.getResult() == ResultCode.UNAUTHORIZED) {
+            this.session.clear();
+            result = ActionSupport.LOGIN;
+        }
+
+        else if(userBookListResponse.getResult() != ResultCode.OK)
+            result = ActionSupport.ERROR;
+
+        return result;
+
+    }
 
     public String extendBorrowing(){
 
-        System.out.println("ISBN = " + getIsbn());
-
-        boolean bookUpdated = false;
-
         List<BookBorrowed> bookList = getBorrowedBooks();
+        LibraryService service = ServiceFactory.getLibraryService();
+        ExtendBorrowing extendBorrowing = new ExtendBorrowing();
+        extendBorrowing.setToken(Token.getToken());
 
         for(BookBorrowed bookBorrowed : bookList){
             if(bookBorrowed.getBook().getIsbn().equals(getIsbn())){
-                LibraryService service = ServiceFactory.getLibraryService();
-                service.extendBorrowing(bookBorrowed);
-                bookUpdated = true;
+                extendBorrowing.setBookBorrowed(bookBorrowed);
+                break;
             }
         }
 
-        if(bookUpdated) return ActionSupport.SUCCESS;
+        if(service.extendBorrowing(extendBorrowing).getResult() == ResultCode.OK)
+            return ActionSupport.SUCCESS;
+
         else return ActionSupport.ERROR;
     }
 
@@ -59,13 +92,16 @@ public class UserAction extends ActionSupport implements SessionAware {
 
     public List<BookBorrowed> getBorrowedBooks() {
 
-        LibraryService service = ServiceFactory.getLibraryService();
+        if(borrowedBooks == null){
+            LibraryService service = ServiceFactory.getLibraryService();
 
-        UserBookList userBookList = new UserBookList();
-        userBookList.setUser(getUser());
+            UserBookList userBookList = new UserBookList();
+            userBookList.setToken(Token.getToken());
 
-        UserBookListResponse userBookListResponse = service.userBookList(userBookList);
-        return userBookListResponse.getBookBorrowed();
+            borrowedBooks = service.userBookList(userBookList).getBookBorrowed();
+        }
+
+        return borrowedBooks;
     }
 
     @Override
