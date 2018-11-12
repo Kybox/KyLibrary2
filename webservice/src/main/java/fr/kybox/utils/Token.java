69a867e45b5be4ba5f684e92c43b9fa7d4da3da9
kybox <nslr@riseup.net>
2018-11-12 18:00:36 +0100

    (Webservice) Add token generator class

--- webservice/src/main/java/fr/kybox/utils/Token.java
+++ webservice/src/main/java/fr/kybox/utils/Token.java
@@ -1,16 +1,43 @@
 package fr.kybox.utils;
 
-import java.util.Base64;
-import java.util.Date;
+import fr.kybox.entities.TokenStorage;
+import fr.kybox.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Component
 public class Token {
 
-    public static String generate(String param1, String param2){
+    @Value("${settings.tokenExpire}")
+    private Integer expInterval;
 
-        String comb = param1 + param2;
-        String e64 = Base64.getEncoder().encodeToString(comb.getBytes());
+    private TokenStorage tokenStorage;
 
-        return UUID.fromString(e64 + new Date().toString()).toString();
+    public void generateNew(UserEntity entity){
+
+        tokenStorage = new TokenStorage();
+        tokenStorage.setCreationDate(LocalDateTime.now());
+        tokenStorage.setExpireDate(getExpirationDate());
+        tokenStorage.setToken(generate());
+        tokenStorage.setUserEntity(entity);
+    }
+
+    private String generate(){
+
+        return String.valueOf(UUID.randomUUID());
+    }
+
+    private LocalDateTime getExpirationDate(){
+
+        System.out.println("ExpInterval = " + expInterval);
+        return LocalDateTime.now().plusMinutes(expInterval);
+    }
+
+    public TokenStorage getTokenStorage(){
+
+        return tokenStorage;
     }
 }
