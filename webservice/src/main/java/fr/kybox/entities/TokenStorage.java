76ec400bc16b35820b46a41651e84affe5c2b84f
kybox <nslr@riseup.net>
2018-11-13 11:22:05 +0100

    Add TokenStorage entity class
    Previous version deleted (why? how?)

---
+++ webservice/src/main/java/fr/kybox/entities/TokenStorage.java
@@ -0,0 +1,57 @@
+package fr.kybox.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "token_storage")
+public class TokenStorage {
+
+    @Id
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "creationDate")
+    private LocalDateTime creationDate;
+
+    @Column(name = "expireDate")
+    private LocalDateTime expireDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    public TokenStorage() {}
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDateTime expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+}
